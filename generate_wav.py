2c3f7499e615fd3e727a9a7401936bf9a9ad9c8d
liulong <thelegendofliulong@gmail.com>
2025-09-23 10:42:22 +0800

    增加日志。方便提取方法

---
+++ generate_wav.py
@@ -0,0 +1,18 @@
+from webui import tts_fn
+
+if __name__ == "__main__":
+    tts_fn(     
+                text,
+                speaker,
+                sdp_ratio,
+                noise_scale,
+                noise_scale_w,
+                length_scale,
+                language,
+                audio_prompt,
+                text_prompt,
+                prompt_mode,
+                style_text,
+                style_weight,
+    )
+           
